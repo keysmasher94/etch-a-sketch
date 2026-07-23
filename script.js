1241e088be7cfbf112d6a5d0231256c7ec53e573
Jared Lynch <jaredlynch@protonmail.com>
2026-07-24 09:30:12 +1000

    First commit; made basic index.html, style.css, script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/* TODO:
+ * - Create a 16x16 set of divs
+ * - Use flexbox (in css) to make the divs appear as a grid
+ * - Set up a hover effect so the grid divs change colour when the mouse goes
+ *   past them
+ * - Add a button at the top that sends the user a popup asking for the number
+ *   of squares per side for the new grid; oncd entered the existing grid will be
+ *   removed and a new grid will be created in the same space (set a maximum of
+ *   100)
+ *   - have the colours be random color values for each square; plus darkening
+ */
+
+const container = document.querySelector(".container");
