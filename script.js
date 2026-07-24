b0560fb1b5b229cf04cf4f830c9bfd380bbdb77e
Jared Lynch <jaredlynch@protonmail.com>
2026-07-24 10:23:06 +1000

    Have modified CSS and JavaScript so the grid looks cleaner

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 /* TODO:
- * - Create a 16x16 set of divs
- * - Use flexbox (in css) to make the divs appear as a grid
  * - Set up a hover effect so the grid divs change colour when the mouse goes
  *   past them
  * - Add a button at the top that sends the user a popup asking for the number
@@ -10,6 +8,10 @@
  *   - have the colours be random color values for each square; plus darkening
  */
 
+// TODO:
+// - Fix margins: they work up until about 60, but then break down after that;
+// they also look a bit too spaced out when you get below 15
+
 /* Create Grid of Squares */
 const container = document.querySelector(".container");
 
@@ -17,10 +19,18 @@ function createGrid(gridSquares, maximum = 100) {
   if (gridSquares <= maximum) {
     for (let i = 0; i < gridSquares; i++) {
       const rowSquare = document.createElement("div");
+      /* Modifying div attributes */
+      rowSquare.style.height = "100%";
+      rowSquare.style.width = "100%";
+      rowSquare.style.margin = `${30 / gridSquares}px`;
       rowSquare.className = "rowSquare";
       for (let j = 0; j < gridSquares; j++) {
         const colSquare = document.createElement("div");
         colSquare.className = "colSquare";
+        /* Modifying div attributes */
+        colSquare.style.width = "100%";
+        colSquare.style.height = "100%";
+        colSquare.style.margin = `${30 / gridSquares}px`;
         rowSquare.appendChild(colSquare);
       }
       container.appendChild(rowSquare);
@@ -30,4 +40,4 @@ function createGrid(gridSquares, maximum = 100) {
   }
 }
 
-createGrid(8);
+createGrid(15);
